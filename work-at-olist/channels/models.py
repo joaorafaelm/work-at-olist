407579fc6641122bfcb4957cbc47895a2571f2c1
João Rafael <joaoraf@me.com>
2017-07-19 00:54:49 -0300

    Changing the post-save behavior of the abstract class
    The abstract class used for the Channel and Category models,
    no longer add an integer to the reference field if it already exists in
    the table. Although the "reference" attribute is set to be unique by
    default, it is more reasonable to treat its uniqueness within the
    respective classes.

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -6,7 +6,7 @@ All models of this module should be defined here.
 
 import uuid
 
-from django.db import IntegrityError, models
+from django.db import models
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 
@@ -17,17 +17,12 @@ class BaseModel(models.Model):
     Models that inherit from this class get an auto filled slug property
     based on the models name property (Along with created/updated time).
 
-    Correctly handles duplicate values (slugs are unique), and truncates slug
-    if value is too long (max_length is 100 by default).
+    Correctly truncates the field to the max_length of the slug field.
 
     The following attributes can be overridden on a per model basis:
     * value_field_name - the value to slugify, default 'name'
     * slug_field_name - the field to store the slugified value in,
                         default 'reference'.
-    * max_interations - how many iterations to search for an open slug before
-                        raising IntegrityError, default 1000.
-    * slug_separator - the character to put in place of spaces and other non
-                       url friendly characters, default '-'.
     """
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -38,46 +33,18 @@ class BaseModel(models.Model):
     # noinspection PyUnresolvedReferences
     def save(self, *args, **kwargs):
         """Auto creates an slugified reference based on another attribute."""
-        pk_field_name = self._meta.pk.name
         value_field_name = getattr(self, 'value_field_name', 'name')
         slug_field_name = getattr(self, 'slug_field_name', 'reference')
-        max_interations = getattr(self, 'slug_max_iterations', 1000)
-        slug_separator = getattr(self, 'slug_separator', '-')
 
-        # fields, query set, other setup variables
+        # Retrieve the field where the slug will be stored
         slug_field = self._meta.get_field(slug_field_name)
         slug_len = slug_field.max_length
-        queryset = self.__class__.objects.all()
 
-        # if the pk of the record is set, exclude it from the slug search
-        current_pk = getattr(self, pk_field_name)
-        if current_pk:
-            queryset = queryset.exclude(**{pk_field_name: current_pk})
-
-        # setup the original slug,
-        # and make sure it is within the allowed length
+        # Slugify the field and make sure it is within the allowed length
         slug = slugify(getattr(self, value_field_name))
-        if slug_len:
-            slug = slug[:slug_len]
-        original_slug = slug
 
-        # iterate until a unique slug is found, or max_iterations
-        counter = 2
-        while queryset.filter(**{
-            slug_field_name: slug
-        }).count() > 0 and counter < max_interations:
-            slug = original_slug
-            suffix = '%s%s' % (slug_separator, counter)
-            if slug_len and len(slug) + len(suffix) > slug_len:
-                slug = slug[:slug_len - len(suffix)]
-            slug = '%s%s' % (slug, suffix)
-            counter += 1
-
-        if counter == max_interations:
-            raise IntegrityError('Unable to locate unique slug.')
-
-        setattr(self, slug_field.attname, slug)
-
+        # Set the slug attribute value
+        setattr(self, slug_field.attname, slug[:slug_len])
         super(BaseModel, self).save(*args, **kwargs)
 
     class Meta:
@@ -137,6 +104,10 @@ class Category(BaseModel):
         verbose_name=_('Parent')
     )
 
+    reference = models.SlugField(_('Reference'), max_length=100)
+
+    value_field_name = 'reference'
+
     class Meta:
         """Django meta class options.
 
@@ -147,6 +118,7 @@ class Category(BaseModel):
         ordering = ['name']
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+        unique_together = ('channel', 'name', 'parent',)
 
     def __str__(self):
         """Return the name attribute as representation."""
