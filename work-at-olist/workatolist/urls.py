8fc3bc53e2da54494988e98bd942d06420304db8
João Rafael <joaoraf@me.com>
2017-07-23 22:38:48 -0300

    Adding homepage redirect to API

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -1,7 +1,9 @@
 """URLs for the project."""
 
 from django.conf.urls import include, url
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^api/v1/', include('channels.urls', namespace='channels')),
+    url(r'^.*$', RedirectView.as_view(url='/api/v1/', permanent=False)),
 ]
